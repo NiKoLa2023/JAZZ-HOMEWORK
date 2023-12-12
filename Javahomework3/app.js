04b9a7e0e2929afbee87415aaff070690fad9054
NiKoLa2023 <150241393+NiKoLa2023@users.noreply.github.com>
2023-12-12 15:21:04 +0100

    Add files via upload

---
+++ Javahomework3/app.js
@@ -0,0 +1,9 @@
+function introduce(one, two, three, four, five){console.log
+    (typeof one, typeof two, typeof three, typeof four, typeof five)}
+    
+    let first
+    let second=null
+    let third="Nikola"
+    let fourth=19
+    let fifth=null===undefined
+    introduce(first, second, third, fourth, fifth)
