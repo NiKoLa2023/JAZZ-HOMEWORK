46601eadcb4302ab50498df81544bf1af8917563
NiKoLa2023 <150241393+NiKoLa2023@users.noreply.github.com>
2023-12-28 12:46:09 +0100

    Add files via upload

---
+++ DOM Homework1/app.js
@@ -0,0 +1,43 @@
+///Homework part1///
+
+let paragraph = document.getElementsByClassName("paragraph")
+
+
+let paragraphOne = paragraph[0]
+
+paragraphOne.innerText = "Newwwwwww Textttttttttttt"
+
+
+
+let paragraphTwo = paragraph[1]
+
+paragraphTwo.innerText = "Some Newwwwwww Textttttttttttt"
+
+
+
+let header = document.getElementsByTagName("h1")
+
+
+
+let headerOne = header[0]
+
+headerOne.innerText = "Newwwwwww Headerrrrrrr"
+
+
+let headerTwo = header[1]
+
+headerTwo.innerText = "Additional Headerrrr"
+
+
+let headers = document.getElementsByTagName("h3")
+
+let headerThree = headers[0]
+
+headerThree.innerText = "Some Newwwwwww Headerrrrrrrrrrr"
+
+
+
+
+
+
+
